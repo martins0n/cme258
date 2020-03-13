7cb6051da7c890c8f3b1a5e7cac738c516535fc0
martins0n <martinmail@mail.ru>
2020-03-14 01:56:08 +0300

    HW: hw1

--- hw/hw1/power_method.cpp
+++ hw/hw1/power_method.cpp
@@ -3,13 +3,41 @@
 #include <iostream>
 #include <fstream>
 #include <random>
+#include <array>
+#include <tuple>
 
 using namespace std;
 
 // FORTRAN adds _ after all the function names
 // and all variables are called by reference
 extern "C"{
-  // Add necessary BLAS functions here.
+    double dsymv_(
+        const char * UPLO, const int * N, 
+        const double * ALPHA, double * A,
+        const int * LDA, double * X, const int * INCX,
+        const double * BETA, double * Y, const int * INCY
+    );
+
+    double dnrm2_(
+        const int * N, double * X, const int * INCX
+    );
+
+    double daxpy_(
+        const int * N, double DA, double * DX, const int * INCX,
+        double * DY, const int * INCY
+    );
+
+    double ddot_(
+        const int * N, double * DX, const int * INCX, double * DY, const int * INCY
+    );
+
+    double dscal_(
+        const int * N, double * DA, double * DX, const int * INCX
+    );
+
+    double dswap_(
+        const int * N, double * DX, const int * INCX, double * DY, const int * INCY
+    );
 }
 
 void read_matrix(ifstream& file, int n, double * A) {
@@ -21,7 +49,59 @@ void read_matrix(ifstream& file, int n, double * A) {
   }
 }
 
+
+
+auto power_method_dsym(int matrix_size, double * matrix, int max_iter=1000){
+    
+    int k = 1;
+    float lambda_old = 0;
+    float lambda = 1;
+    float eps = 1e-6;
+    random_device rd;
+    mt19937 gen(rd());
+    std::uniform_real_distribution<double> unif(-1., 1.);
+    auto x = (double *) malloc(matrix_size * sizeof(double));
+    auto y = (double *) malloc(matrix_size * sizeof(double));
+    for(int i = 0; i < matrix_size; i++){
+        x[i] = unif(gen);
+    
+    for(int i = 0; i < matrix_size; i++){
+        y[i] = unif(gen);
+    }}
+
+    const char UPLO = 'u';
+    const int INCX = 1;
+    const int INCY = 1;
+    const int N = matrix_size;
+    const double ALPHA = 1;
+    const double BETA = 0;
+    double euc_norm = 0;
+    
+    while (abs(lambda - lambda_old) > eps && k < max_iter){
+        
+        lambda_old = lambda;
+
+        dsymv_(
+            &UPLO, &N, &ALPHA, matrix, &N, x, &INCX, &BETA, y, &INCY
+        );
+
+        lambda = ddot_(&N, y, &INCX, x, &INCY) / dnrm2_(&N, x, &INCX);
+
+        euc_norm = 1 / dnrm2_(&N, y, &INCX);
+
+        dscal_(&N, &euc_norm, y, &INCX);
+
+        dswap_(&N, x, &INCX, y, &INCY);
+        
+        k++;
+
+    }
+
+    return std::make_tuple(lambda, x);
+}
+
 int main( int argc, char** argv ){
+    
   if (argc != 2) {
     printf("usage: power_method matrix\n");
     printf("  matrix should be text file of form:\n");
@@ -32,25 +112,21 @@ int main( int argc, char** argv ){
   }
 
   // Get matrix
-  ifstream mat_file( argv[1] );
+  ifstream mat_file(argv[1]);
   if (!mat_file) {
     printf("Could not open file.");
     return 1;
   }
-
   int n;
   mat_file >> n;
+  
   double *A = (double *) malloc(n * n * sizeof(double));
   read_matrix(mat_file, n, A);
 
-  // Random number generator
-  random_device rd;
-  mt19937 gen(rd());
-  std::uniform_real_distribution<double> unif(-1., 1.);
-
-  // implement power method here
-  double lambda = 0;
-  double *x = (double *) calloc(n, sizeof(double));
+  double lambda; 
+  double * x;
+  
+  std::tie(lambda, x) = power_method_dsym(n, A);
 
   // Print results
   printf("Eigenvalue : %f\n", lambda);
